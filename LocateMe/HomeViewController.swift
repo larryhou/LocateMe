6f078509f8ae38b08935606ef4c078593c23cbd0
larryhou <larryhou@foxmail.com>
2014-08-15 19:19:18 +0800

    交互优化

--- LocateMe/HomeViewController.swift
+++ LocateMe/HomeViewController.swift
@@ -14,12 +14,6 @@ class HomeViewController: UITabBarController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    @IBAction func returnHomeByUnwind(segue:UIStoryboardSegue)
-    {
-        
     }
 }
 
